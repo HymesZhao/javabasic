f454bb9ffc7c4571e1fccf4d08f6d6d445746075
hymesZhao <hymes.zhao@foxmail.com>
2020-12-08 00:41:52 +0800

    完善代码

--- JavaSenior/day13/src/com/atguigu/java2/ThreadNew.java
+++ JavaSenior/day13/src/com/atguigu/java2/ThreadNew.java
@@ -17,10 +17,10 @@ import java.util.concurrent.FutureTask;
  * @create 2019-02-15 下午 6:01
  */
 //1.创建一个实现Callable的实现类
-class NumThread implements Callable{
+class NumThread implements Callable<Integer>{
     //2.实现call方法，将此线程需要执行的操作声明在call()中
     @Override
-    public Object call() throws Exception {
+    public Integer call() throws Exception {
         int sum = 0;
         for (int i = 1; i <= 100; i++) {
             if(i % 2 == 0){
@@ -38,7 +38,7 @@ public class ThreadNew {
         //3.创建Callable接口实现类的对象
         NumThread numThread = new NumThread();
         //4.将此Callable接口实现类的对象作为传递到FutureTask构造器中，创建FutureTask的对象
-        FutureTask futureTask = new FutureTask(numThread);
+        FutureTask<Integer> futureTask = new FutureTask<>(numThread);
         //5.将FutureTask的对象作为参数传递到Thread类的构造器中，创建Thread对象，并调用start()
         new Thread(futureTask).start();
 
@@ -47,9 +47,7 @@ public class ThreadNew {
             //get()返回值即为FutureTask构造器参数Callable实现类重写的call()的返回值。
             Object sum = futureTask.get();
             System.out.println("总和为：" + sum);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
+        } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
     }
--- JavaSenior/day14/src/com/atguigu/exer/StringTest.java
+++ JavaSenior/day14/src/com/atguigu/exer/StringTest.java
@@ -9,16 +9,51 @@ public class StringTest {
 
     String str = new String("good");
     char[] ch = { 't', 'e', 's', 't' };
+    int a=3;
+    People people=new People("zhangsan",3);
 
-    public void change(String str, char ch[]) {
+    public void change(String str, char ch[],int a,People people) {
         str = "test ok";
         ch[0] = 'b';
+        a=4;
+        people.setAge(4);
+        people.setName ("lisi") ;
     }
     public static void main(String[] args) {
         StringTest ex = new StringTest();
-        ex.change(ex.str, ex.ch);
+        ex.change(ex.str, ex.ch,ex.a,ex.people);
         System.out.println(ex.str);//good
         System.out.println(ex.ch);//best
+        System.out.println(ex.a);//3
+        System.out.println(ex.people.age+","+ex.people.name);//3
+    }
+}
+class People{
+    String name;
+    int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public People(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public People() {
     }
 }
 
